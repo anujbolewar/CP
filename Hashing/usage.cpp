37e1ddcc29d7c3088e699ea55c98cc37d91f159f
anujbolewar <bolewara@gmail.com>
2025-09-19 23:32:41 +0530

    Add  dp ,bit manipulation files

---
+++ Hashing/usage.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    vector<string> names = {"alice", "brad", "collin", "brad", "dylan", "kim"};
+    unordered_map<string, int> countMap;
+
+    for (string& name: names) {
+        if (countMap.count(name) == 0) {
+            countMap[name] = 1;
+        } else {
+            countMap[name]++;
+        }
+    }
+
+    return 0;
+}
